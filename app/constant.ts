5b0c6770eae754b421b7f95c1a2e650d472ea9ad
zhangolve <zhangolve@users.noreply.github.com>
2024-07-23 17:27:11 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -298,15 +298,15 @@ export const DEFAULT_MODELS = [
       providerType: "google",
     },
   })),
-  ...anthropicModels.map((name) => ({
-    name,
-    available: true,
-    provider: {
-      id: "anthropic",
-      providerName: "Anthropic",
-      providerType: "anthropic",
-    },
-  })),
+  // ...anthropicModels.map((name) => ({
+  //   name,
+  //   available: true,
+  //   provider: {
+  //     id: "anthropic",
+  //     providerName: "Anthropic",
+  //     providerType: "anthropic",
+  //   },
+  // })),
   ...baiduModels.map((name) => ({
     name,
     available: true,
